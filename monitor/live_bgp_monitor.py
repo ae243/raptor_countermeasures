b295f1718a589f5fae308c89acd8aa0dbb78da71
ae243 <annee@cs.princeton.edu>
2015-11-17 15:01:14 -0500

    added bgp monitoring and paper modifications

--- monitor/live_bgp_monitor.py
+++ monitor/live_bgp_monitor.py
@@ -1,10 +1,16 @@
+import sys
 from _pybgpstream import BGPStream, BGPRecord, BGPElem
+
+pfx_list = []
+f = open(sys.argv[1], 'r')
+for line in f:
+    pfx_list.append(line.strip())
 
 stream = BGPStream()
 rec = BGPRecord()
 
-stream.add_filter('collector', 'rrc11')
-stream.add_interval_filter(0,0)
+stream.add_filter('collector', 'rrc00')
+stream.add_interval_filter(1438415400,-1)
 stream.set_live_mode()
 
 stream.start()
@@ -15,8 +21,10 @@ while(stream.get_next_record(rec)):
     else:
         elem = rec.get_next_elem()
         while(elem):
-            print rec.project, rec.collector, rec.type, rec.time, rec.status,
-            print elem.type, elem.peer_address, elem.peer_asn, elem.fields
-            elem = rec.get_next_elem()
+            pfx = elem.fields['prefix']
+            if pfx in pfx_list:
+                print rec.project, rec.collector, rec.type, rec.time, rec.status,
+                print elem.type, elem.peer_address, elem.peer_asn, elem.fields
+                elem = rec.get_next_elem()
 
 
