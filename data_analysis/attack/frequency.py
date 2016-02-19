512d56204d78909c1b6bdd26aeaa047d9cc31b02
ae243 <annee@cs.princeton.edu>
2016-02-18 20:05:10 -0500

    updated section 5

---
+++ data_analysis/attack/frequency.py
@@ -0,0 +1,41 @@
+import sys
+
+# usage: python frequency.py <threshold numerator>
+# Calculates the frequency of an AS announcing a path to prefix p - if the frequency is below a threshold, then flag it.
+
+f = open('tor_log.txt', 'r')
+count_map = {} # <(prefix,originAS) -> count>
+total_map = {} # <prefix -> number of total announcements>
+for line in f:
+    if line.split(" ")[5] == 'A':
+        prefix = line.split(" ")[11][1:-2].strip()
+        origin_as = line.split("'as-path': '")[1][:-3].split(" ")[-1].strip()
+        if (prefix,origin_as) in count_map:
+            count_map[(prefix,origin_as)] += 1
+        else:
+            count_map[(prefix,origin_as)] = 1
+
+        if prefix in total_map:
+            total_map[prefix] += 1
+        else:
+            total_map[prefix] = 1
+
+unique_asns = []
+unique_prefs = []
+freq_map = {}
+fp = 0
+for x in count_map:
+    pref = x[0]
+    total = total_map[pref]
+    count = count_map[x]
+    fraction = float(float(count)/float(total))
+    freq_map[x] = fraction
+#    if fraction < float(float(sys.argv[1])/float(total_map[pref])):
+    if fraction <= float(sys.argv[1]):
+        fp += 1
+        print x[0], x[1]
+        unique_prefs.append(x[0])
+        unique_asns.append(x[1])
+print fp
+prefs = list(set(unique_prefs))
+asns = list(set(unique_asns))
---
+++ data_analysis/attack/time.py
@@ -0,0 +1,58 @@
+import sys
+
+# usage: python time.py <threshold value>
+
+f = open('tor_log.txt', 'r')
+time_map = {} # <(prefix,originAS) -> time announced>
+bool_map = {} # <(prefix,originAS) -> '' or latest announcement time>
+pref_map = {} # <pref -> latest_asn>
+last_time = ''
+total_time = {} # <pref -> total_announced_time>
+for line in f:
+    if line.split(" ")[5].strip() == 'A':
+        pref = line.split(" ")[11][1:-2].strip()
+        asn = line.split("'as-path': '")[1][:-3].split(" ")[-1].strip()
+        ts = line.split(" ")[3].strip() 
+        if (not (pref,asn) in bool_map) or bool_map[(pref,asn)] == '':
+            bool_map[(pref,asn)] = ts
+            pref_map[pref] = asn
+    else:
+        pref = line.split(" ")[9][1:-3].strip()
+        if pref in pref_map:
+            asn = pref_map[pref]
+            if ((pref,asn) in bool_map) and bool_map[(pref,asn)] != '':
+                asn = pref_map[pref]
+                ts = line.split(" ")[3].strip()
+                new_ts = float(ts) - float(bool_map[(pref,asn)])
+                if (pref,asn) in time_map:
+                    time_map[(pref,asn)] += new_ts
+                else:
+                    time_map[(pref,asn)] = new_ts
+                bool_map[(pref,asn)] = ''
+    last_time = line.split(" ")[3].strip()
+
+for b in bool_map:
+    if bool_map[b] != '':
+        if b in time_map:
+            time_map[b] += (float(last_time) - float(bool_map[b]))
+        else:
+            time_map[b] = (float(last_time) - float(bool_map[b]))
+
+total = 0
+for t in time_map:
+    pref = t[0]
+    if pref in total_time:
+        total_time[pref] += time_map[t]
+    else:
+        total_time[pref] = time_map[t]
+    total += 1
+print total
+
+fp = 0
+for x in time_map:
+    pref = x[0]
+    if (float(total_time[pref]) != float(0.0)) and (float(time_map[x]) != float(0.0)) and float(float(time_map[x])/float(total_time[pref])) < float(sys.argv[1]):
+        print x[0], x[1]
+        fp += 1
+
+print fp
